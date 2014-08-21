c42e78f8c03895e025f44e234d5ff4d02c1a6929
Vinicius Herbstrith <viniherbstrith@gmail.com>
2014-08-21 14:39:10 -0300

    poti_header implementation and rst_read changes

--- librastro/src/rst_read.c
+++ librastro/src/rst_read.c
@@ -475,11 +475,14 @@ void rst_print_event(rst_event_t *event)
 {
   int i;
   if (event->file->resolution > RST_MICROSECONDS){
-    printf("%d type: %d ts: %.9f (id1=%"PRIu64",id2=%"PRIu64")\n", event->file->id,
-           event->type, event->timestamp, event->id1, event->id2);
+    /*printf("%d type: %d ts: %.9f (id1=%"PRIu64",id2=%"PRIu64")\n", event->file->id,
+           event->type, event->timestamp, event->id1, event->id2);*/
+    printf("file %d event-type: %d \n", event->file->id, event->type);
+
   }else{
-    printf("%d type: %d ts: %f (id1=%"PRIu64",id2=%"PRIu64")\n", event->file->id,
-           event->type, event->timestamp, event->id1, event->id2);
+    /*printf("%d type: %d ts: %f (id1=%"PRIu64",id2=%"PRIu64")\n", event->file->id,
+           event->type, event->timestamp, event->id1, event->id2);*/
+    printf("file %d event-type: %d \n", event->file->id,event->type);
   }
   if (event->ct.n_uint64 > 0) {
     printf("\tu_int64_ts-> ");
