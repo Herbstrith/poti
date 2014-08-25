aa7de709cc168cf71c2d501b2460d4684d7c15d6
Herbstrith <viniherbstrith@gmail.com>
2014-08-24 21:57:43 -0300

    poti_reader converting the binary header part to paje header standard

--- librastro/src/rst_read.c
+++ librastro/src/rst_read.c
@@ -475,14 +475,11 @@ void rst_print_event(rst_event_t *event)
 {
   int i;
   if (event->file->resolution > RST_MICROSECONDS){
-    /*printf("%d type: %d ts: %.9f (id1=%"PRIu64",id2=%"PRIu64")\n", event->file->id,
-           event->type, event->timestamp, event->id1, event->id2);*/
-    printf("file %d event-type: %d \n", event->file->id, event->type);
-
+    printf("%d type: %d ts: %.9f (id1=%"PRIu64",id2=%"PRIu64")\n", event->file->id,
+           event->type, event->timestamp, event->id1, event->id2);
   }else{
-    /*printf("%d type: %d ts: %f (id1=%"PRIu64",id2=%"PRIu64")\n", event->file->id,
-           event->type, event->timestamp, event->id1, event->id2);*/
-    printf("file %d event-type: %d \n", event->file->id,event->type);
+    printf("%d type: %d ts: %f (id1=%"PRIu64",id2=%"PRIu64")\n", event->file->id,
+           event->type, event->timestamp, event->id1, event->id2);
   }
   if (event->ct.n_uint64 > 0) {
     printf("\tu_int64_ts-> ");
--- src/poti_reader.c
+++ src/poti_reader.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <argp.h>
 #include <rastro.h>
+#include "poti_private.h"
 
 static char doc[] = "Reads the poti binary trace";
 static char args_doc[] = "{rastro-0-0.rst rastro-1-0.rst ...}";
@@ -16,6 +17,10 @@ struct arguments {
   char *synchronization_file;
 };
 
+
+void poti_print_event(rst_event_t *event);
+const char* eventidToString(u_int32_t event);
+const char* fieldToString(u_int32_t event);
 static int parse_options (int key, char *arg, struct argp_state *state)
 {
   struct arguments *arguments = state->input;
@@ -41,9 +46,224 @@ static int parse_options (int key, char *arg, struct argp_state *state)
 
 static struct argp argp = { options, parse_options, args_doc, doc };
 
-void poti_print_event (rst_event *event)
+const char* eventidToString(u_int32_t event)
 {
-  //implement here
+
+	switch (event)
+	{
+		case PajeDefineContainerTypeEventId:
+			return "PajeDefineContainerType";
+			break;
+		case PajeDefineEventTypeEventId:
+			return "PajeDefineEventType";
+			break;
+		case PajeDefineStateTypeEventId:
+			return "PajeDefineStateType";
+			break;
+		case PajeDefineVariableTypeEventId:
+			return "PajeDefineVariableType";
+			break;
+		case PajeDefineLinkTypeEventId:
+			return "PajeDefineLinkType";
+			break;
+		case PajeDefineEntityValueEventId:
+			return "PajeDefineEntityValue";
+			break;
+		case PajeCreateContainerEventId:
+			return "PajeCreateContainer";
+			break;
+		case PajeDestroyContainerEventId:
+			return "PajeDestroyContainer";
+			break;
+		case PajeNewEventEventId:
+			return "PajeNewEventEvent";
+			break;
+		case PajeSetStateEventId:
+			return "PajeSetState";
+			break;
+		case PajePushStateEventId:
+			return "PajePushState";
+			break;	
+		case PajePopStateEventId:
+			return "PajePopState";
+			break;	
+		case PajeResetStateEventId:
+			return "PajeResetState";
+			break;
+		case PajeSetVariableEventId:
+			return "PajeSetVariable";
+			break;	
+		case PajeAddVariableEventId:
+			return "PajeAddVariable";
+			break;		
+		case PajeSubVariableEventId:
+			return "PajeSubVariable";
+			break;	
+		case PajeStartLinkEventId:
+			return "PajeStartLink";
+			break;
+		case PajeEndLinkEventId:
+			return "PajeEndLink";
+			break;
+		case PajeEventIdCount:
+			return "PajeEventIdCount";
+			break;
+		case PajeUnknownEventId:
+			return "PajeUnknownEventId";
+			break;	
+		
+	}
+	return "error on event def";
+}
+
+const char* fieldToString(u_int32_t event)
+{
+	switch (event)
+	{
+		case PAJE_Time:
+			return  "Time date";
+			break;
+		case PAJE_Name:
+			return "Name string";
+			break;
+		case PAJE_Type:
+			return "Type string";
+			break;
+		case PAJE_Container:
+			return "Container string";
+			break;
+		case PAJE_StartContainerType:
+			return "StartContainerType string";
+			break;
+		case PAJE_EndContainerType:
+			return "EndContainerType string";
+			break;
+		case PAJE_StartContainer:
+			return "StartContainer string";
+			break;
+		case PAJE_EndContainer:
+			return "EndContainer string";
+			break;
+		case PAJE_Value:
+			return "Value string";
+			break;
+		case PAJE_Key:
+			return "Key string";
+			break;
+		case PAJE_Alias:
+			return "Alias string";
+			break;	
+		case PAJE_Color:
+			return "Color color";
+			break;
+/*	
+		case PAJE_Line:
+			return "xx";
+			break;
+		case PAJE_File:
+			return "xx";
+			break;	
+		case PAJE_Extra:
+			return "xx";
+			break;		
+		case PAJE_Unknown_Field:
+			return "xx";
+			break;	
+*/
+		
+	}
+	return "error on field def";
+}
+//reads the binary rst events and print them in  paje  format
+void poti_print_event (rst_event_t *event)
+{
+
+
+	//header event
+	if(event->type == 0 )
+	{
+		int i = 0;
+		printf("%%EventDef %s %" PRIu32 " \n", eventidToString(event->v_uint32[i]), event->v_uint32[i]);
+		i++;
+		for (i = 0; i < event->ct.n_uint32; i++) {
+			printf("%%       %s\n", fieldToString(event->v_uint32[i]));
+		}
+		printf("%%EndEventDef\n");
+	  	
+
+	}
+	//extended header event
+	/*if(event->type == 1)
+	{
+		if (event->ct.n_string > 0) {
+		    printf("\tstrings-> ");
+		    for (i = 0; i < event->ct.n_string; i++) {
+		      printf("(%s) ", event->v_string[i]);
+		    }
+		    printf("\n");
+		  }
+
+	}*/
+
+/*
+	  int i;
+	  if (event->file->resolution > RST_MICROSECONDS){
+	    printf("%d type: %d ts: %.9f (id1=%"PRIu64",id2=%"PRIu64")\n", event->file->id,
+		   event->type, event->timestamp, event->id1, event->id2);
+	  }else{
+	    printf("%d type: %d ts: %f (id1=%"PRIu64",id2=%"PRIu64")\n", event->file->id,
+		   event->type, event->timestamp, event->id1, event->id2);
+	  }
+	  if (event->ct.n_uint64 > 0) {
+	    printf("\tu_int64_ts-> ");
+	    for (i = 0; i < event->ct.n_uint64; i++) {
+	      printf("(%" PRIu64 ") ", event->v_uint64[i]);
+	    }
+	    printf("\n");
+	  }
+	  if (event->ct.n_string > 0) {
+	    printf("\tstrings-> ");
+	    for (i = 0; i < event->ct.n_string; i++) {
+	      printf("(%s) ", event->v_string[i]);
+	    }
+	    printf("\n");
+	  }
+	  if (event->ct.n_float > 0) {
+	    printf("\tfloats-> ");
+	    for (i = 0; i < event->ct.n_float; i++) {
+	      printf("(%f) ", event->v_float[i]);
+	    }
+	    printf("\n");
+	  }
+	  if (event->ct.n_uint32 > 0) {
+	    printf("\tu_int32_ts-> ");
+	    for (i = 0; i < event->ct.n_uint32; i++) {
+	      printf("(%d) ", event->v_uint32[i]);
+	    }
+	    printf("\n");
+	  }
+	  if (event->ct.n_uint16 > 0) {
+	    printf("\tu_int16_ts-> ");
+	    for (i = 0; i < event->ct.n_uint16; i++) {
+	      printf("(%d) ", event->v_uint16[i]);
+	    }
+	    printf("\n");
+	  }
+	  if (event->ct.n_uint8 > 0) {
+	    printf("\tu_int8_ts-> ");
+	    for (i = 0; i < event->ct.n_uint8; i++) {
+	      printf("(%c) ", event->v_uint8[i]);
+	    }
+	    printf("\n");
+	  }
+	  if (event->ct.n_double > 0) {
+	    printf("\tdoubles-> ");
+	    for (i = 0; i < event->ct.n_double; i++) {
+	      printf("(%f) ", event->v_double[i]);
+	    }
+	    printf("\n");
+	  }
+*/
 }
 
 int main (int argc, char *argv[])
