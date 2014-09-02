46cbbf6ebb57cd16b37982f206d1cda2bd75c2ce
Lucas Schnorr <schnorr@inf.ufrgs.br>
2014-09-02 14:12:42 -0300

    changing example to reflect a future idea of API to support user-defined events

--- examples/main_example.c
+++ examples/main_example.c
@@ -23,6 +23,7 @@ int main (int argc, char **argv)
   //second parameter is a boolean indicating an old header or not
 
   poti_header (0, 0);
+  int new_push_state_identifier = poti_header_PajePushState (old_header, "Mark", PAJE_string);
 
   //Defining my types
   poti_DefineContainerType ("ROOT", "0", "ROOT");
@@ -55,6 +56,8 @@ int main (int argc, char **argv)
   poti_PushState (0.89, "thread-1", "STATE", "i");
   poti_PopState  (0.90, "thread-0", "STATE");
   poti_PopState  (1.01, "thread-1", "STATE");
+
+  poti_UserPushState (new_push_state_identifier, 0.34, "thread-0", "STATE", "r", "valor_do_mark");
 
   //End the link at thread-1
   poti_EndLink (1.02, "root", "LINK", "thread-1", "c", "myKey-001");
