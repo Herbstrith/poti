d4a44d0af4b0afa58a389c350d9ba6d71e94d0a6
Lucas Mello Schnorr <schnorr@inf.ufrgs.br>
2015-05-26 23:48:45 -0300

    changes to accept better parameters

--- src/paje2rst/main.c
+++ src/paje2rst/main.c
@@ -14,8 +14,47 @@
     You should have received a copy of the GNU Public License
     along with Poti. If not, see <http://www.gnu.org/licenses/>.
 */
+#include <argp.h>
 #include "main.h"
 
+static char doc[] = "Translates from textual to binary Paje file format";
+static char args_doc[] = "{file.paje}";
+
+static struct argp_option options[] = {
+  { 0 }
+};
+
+#define MAX_INPUT_SIZE 1
+struct arguments {
+  char *input[MAX_INPUT_SIZE];
+  int input_size;
+};
+
+static int parse_options (int key, char *arg, struct argp_state *state)
+{
+  struct arguments *arguments = (struct arguments*)state->input;
+  switch (key){
+  case ARGP_KEY_ARG:
+    if (arguments->input_size == MAX_INPUT_SIZE) {
+      /* Too many arguments. */
+      argp_usage (state);
+    }
+    arguments->input[state->arg_num] = arg;
+    arguments->input_size++;
+    break;
+  case ARGP_KEY_END:
+    /* if (state->arg_num < 0) */
+    /*   /\* Not enough arguments. *\/ */
+    /*   argp_usage (state); */
+    break;
+  default: return ARGP_ERR_UNKNOWN;
+  }
+  return 0;
+}
+
+static struct argp argp = { options, parse_options, args_doc, doc };
+
+extern FILE* yyin;
 void yyerror (char const *s);
 void yyerror (char const *s) {
   fprintf (stderr, "%s\n", s);
@@ -23,6 +62,27 @@ void yyerror (char const *s) {
 
 int main (int argc, char **argv)
 {
+  struct arguments arguments;
+  bzero (&arguments, sizeof(struct arguments));
+  if (argp_parse (&argp, argc, argv, 0, 0, &arguments) == ARGP_KEY_ERROR){
+    fprintf(stderr,
+            "[paje2rst] at %s, "
+            "error during the parsing of parameters\n",
+            __FUNCTION__);
+    return 1;
+  }
+
+  if (arguments.input_size > 0){
+    yyin = fopen (arguments.input[0], "r");
+    if (yyin == NULL){
+      fprintf(stderr,
+	      "[paje2rst] at %s, "
+	      "cannot open file %s\n",
+	      __FUNCTION__, arguments.input[0]);
+      return 1;
+    }
+  }
+  
   poti_init(POTI_BINARY, "output.rst", -1, -1);
   int ret = yyparse();
   poti_close();
